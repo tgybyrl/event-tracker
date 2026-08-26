4bdf531485d934767ec56fdf3bffcd2d7144aa6a
tgybyrl <tgybyrl48@gmail.com>
2026-08-26 16:59:51 +0300

    Created test sql codes. These are just tests and notes for later use.

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+CREATE TABLE events (
+    event_id CHAR(36) PRIMARY KEY,
+    event_platform VARCHAR(50) NOT NULL,
+    event_payload JSON NOT NULL
+);
+
+INSERT INTO events (event_id, event_platform, event_payload) VALUES ('123e21312','web', '{"action": "add_to_cart", "product_id": 123}');
+
+INSERT INTO events (event_id, event_platform, event_payload) VALUES ('231e432112','mobile', '{"action": "add_to_cart", "product_id": 124}');
+
+
+
+SELECT * FROM events;
+
+DROP TABLE events;

1c064ee51f8698a96f02ce30a79b5f3c4a8349eb
tgybyrl <tgybyrl48@gmail.com>
2026-08-31 14:44:24 +0300

    Updated to work with SQL.

--- backend/models/event.go
+++ backend/models/event.go
@@ -1,6 +1,15 @@
 package models
 
+import "time"
+
 type Event struct {
-	Title  string `json:"title"`
-	Detail string `json:"detail"`
+	E_ID      string      `json:"event_id"`
+	U_ID      int         `json:"user_id"`   
+	Platform  string      `json:"event_platform"`
+	Domain    string      `json:"event_domain"`
+	Source    string      `json:"event_source"`
+	Action    string      `json:"event_action"`
+	Location  string      `json:"event_location"`
+	Payload   any         `json:"event_payload"`
+	Timestamp time.Time   `json:"event_timestamp"`
 }

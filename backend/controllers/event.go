a5833f92eb235ee77c897ea8c4bcebc4a433c658
tgybyrl <tgybyrl48@gmail.com>
2026-09-09 09:14:06 +0300

    fix(event): fixed event struct, exec and made err return.
    Changed Payload type to json.RawMessage to make it byte type. Removed newEvent.Timestamp from config.DB.Exec(..) because it was returning 01.01.0001 like min value, now MySQL handles it. Added return to err and success.

--- backend/controllers/event.go
+++ backend/controllers/event.go
@@ -12,18 +12,21 @@ func CreateEvent(c *gin.Context) {
 	
 	var newEvent models.Event
 
-	c.ShouldBindJSON(&newEvent)
+	if err := c.ShouldBindJSON(&newEvent); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 	
-	fmt.Println(newEvent)
-	
-	query:=`INSERT INTO events (event_id, user_id, user_ip, event_platform, event_domain, event_source, event_action, event_payload, event_timestamp) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	query:=`INSERT INTO events (event_id, user_id, user_ip, event_platform, event_domain, event_source, event_action, event_payload) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`
 
-	_, err := config.DB.Exec(query, newEvent.EventID, newEvent.UserID, newEvent.UserIP, newEvent.Platform, newEvent.Domain, newEvent.Source, newEvent.Action, newEvent.Payload, newEvent.Timestamp)
+	_, err := config.DB.Exec(query, newEvent.EventID, newEvent.UserID, newEvent.UserIP, newEvent.Platform, newEvent.Domain, newEvent.Source, newEvent.Action, newEvent.Payload)
 
 	if err != nil{
 		c.JSON(500, gin.H{"error": err.Error()})
-		}
-	
+		return		
+	}
+	c.JSON(201, gin.H{"event_id": newEvent.EventID})
+
 }
 
 func SayHello(c *gin.Context) {
--- backend/models/event.go
+++ backend/models/event.go
@@ -1,15 +1,17 @@
 package models
 
-import "time"
+import ("time",
+				"encode/json"
+)
 
 type Event struct {
-	EventID      string      `json:"event_id"`
-	UserID      *int        `json:"user_id"`
-	UserIP    *string     `json:"user_ip"`   
-	Platform  string      `json:"event_platform"`
-	Domain    string      `json:"event_domain"`
-	Source    string      `json:"event_source"`
-	Action    string      `json:"event_action"`
-	Payload   any         `json:"event_payload"`
-	Timestamp time.Time   `json:"event_timestamp"`
+	EventID   string      		`json:"event_id"`
+	UserID    *int        		`json:"user_id"`
+	UserIP    *string     		`json:"user_ip"`   
+	Platform  string      		`json:"event_platform"`
+	Domain    string      		`json:"event_domain"`
+	Source    string      		`json:"event_source"`
+	Action    string      		`json:"event_action"`
+	Payload   json.RawMessage `json:"event_payload"`
+	Timestamp time.Time       `json:"event_timestamp"`
 }
